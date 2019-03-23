49429978cb6df8c766616010aeee7dde88652127
drjknowles <41568009+drjknowles@users.noreply.github.com>
2019-03-23 21:23:41 +0000

    Updated to add loop functionality. error handling and code cleanup

--- pwned-search.cs
+++ pwned-search.cs
@@ -1,49 +1,70 @@
-using System;
-using System.Security.Cryptography;
-using System.Text;
-using System.Net;
-using System.IO;
+namespace pwned_search
+{
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Security.Cryptography;
+    using System.Text;
 
-namespace pwned_search {
-    class Program {
-        static void Main(string[] args) {
-            Console.Write("Enter the password to check: ");
-            string plaintext = Console.ReadLine();
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Enter the password to check or 'x' to exit: ");
 
-            SHA1 sha = new SHA1CryptoServiceProvider();
-            byte[] data = sha.ComputeHash(Encoding.ASCII.GetBytes(plaintext));
+            var input = Console.ReadLine();
 
-            // Loop through each byte of the hashed data 
-            // and format each one as a hexadecimal string.
-            var sBuilder = new StringBuilder();
-            for (int i = 0; i < data.Length; i++) {
-                sBuilder.Append(data[i].ToString("x2"));
-            }
-            string result = sBuilder.ToString().ToUpper();
-            Console.WriteLine($"The SHA-1 hash of {plaintext} is: {result}");
+            while (input != "x")
+            {
+                SHA1 sha = new SHA1CryptoServiceProvider();
+                var data = sha.ComputeHash(Encoding.ASCII.GetBytes(input));
 
-            // get a list of all the possible passwords where the first 5 digits of the hash are the same
-            string url = "https://api.pwnedpasswords.com/range/" + result.Substring(0, 5);
-            WebRequest request= WebRequest.Create(url);
-            Stream response = request.GetResponse().GetResponseStream();
-            StreamReader reader = new StreamReader(response);
+                // Loop through each byte of the hashed data 
+                // and format each one as a hexadecimal string.
+                var sBuilder = new StringBuilder();
+                for (var i = 0; i < data.Length; i++) sBuilder.Append(data[i].ToString("x2"));
+                var result = sBuilder.ToString().ToUpper();
+                Console.WriteLine($"The SHA-1 hash of {input} is: {result}");
 
-            // look at each possibility and compare the rest of the hash to see if there is a match
-            string hashToCheck = result.Substring(5);
-            while (true) {
-                string line = reader.ReadLine();
-                if (line == null) {
-                    Console.WriteLine("That password was not found.");
-                    break;
+                try
+                {
+                    // get a list of all the possible passwords where the first 5 digits of the hash are the same
+                    var url = "https://api.pwnedpasswords.com/range/" + result.Substring(0, 5);
+                    var request = WebRequest.Create(url);
+                    var response = request.GetResponse().GetResponseStream();
+                    var reader = new StreamReader(response);
+
+                    // look at each possibility and compare the rest of the hash to see if there is a match
+                    var hashToCheck = result.Substring(5);
+
+                    var resultCount = 0;
+                    var timesFound = 0;
+
+                    var line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        var parts = line.Split(':');
+
+                        resultCount++;
+
+                        if (parts[0] == hashToCheck) timesFound = int.Parse(parts[1]);
+
+                        line = reader.ReadLine();
+                    }
+
+                    Console.WriteLine($"Search result returned {resultCount} matching hashes.");
+                    Console.WriteLine($"Password has been compromised {timesFound} times.");
                 }
-                string[] parts = line.Split(':');
-                if (parts[0] == hashToCheck) {
-                    Console.WriteLine("Password has been compromised -- DO NOT USE!");
-                    break;
+                catch
+                {
+                    Console.WriteLine("An error occured querying pwnedpasswords.com API.");
                 }
+
+                Console.Write("Enter another password to check for 'x' to exit: ");
+
+                input = Console.ReadLine();
             }
-            
-            Console.ReadKey(); // pause until key press
         }
     }
 }
